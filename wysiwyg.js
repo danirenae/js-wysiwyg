a23005eeeca63eb89eb81056aeb06f0da5b8f8b0
Dani Adkins <danirenaeadkins12@gmail.com>
2016-09-14 21:33:07 -0500

    added initial files

---
+++ wysiwyg.js
@@ -0,0 +1,48 @@
+var famousPeople = [{
+  title: "Samurai",
+  name: "Tomoe Gozen",
+  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg"
+  // lifespan: {
+    birth: 1747,
+    death: 1797
+  }
+}, {
+  title: "Dorothy",
+  name: "Judy Garland",
+  bio: "Her house is uprooted in a tornado in Kansas and she is tranported to a fantasy world.",
+  image: "http://www.jgdb.com/oz20.jpg",
+  lifespan: {
+    birth: 1922,
+    death: 1969
+  }
+}, {
+  title: "Dorothy",
+  name: "Judy Garland",
+  bio: "Her house is uprooted in a tornado in Kansas and she is tranported to a fantasy world.",
+  image: "http://www.jgdb.com/oz20.jpg",
+  lifespan: {
+    birth: 1922,
+    death: 1969
+  }
+},  {
+  title: "Dorothy",
+  name: "Judy Garland",
+  bio: "Her house is uprooted in a tornado in Kansas and she is tranported to a fantasy world.",
+  image: "http://www.jgdb.com/oz20.jpg",
+  lifespan: {
+    birth: 1922,
+    death: 1969
+  }
+}, {
+title: "Dorothy",
+  name: "Judy Garland",
+  bio: "Her house is uprooted in a tornado in Kansas and she is tranported to a fantasy world.",
+  image: "http://www.jgdb.com/oz20.jpg",
+  lifespan: {
+    birth: 1922,
+    death: 1969
+  }
+}];
+
+console.log("hey")
